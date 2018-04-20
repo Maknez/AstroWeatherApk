d75c0e647cbd86229b487ad2a74b468ad34aa336
Maknez <themaknez@gmail.com>
2018-04-21 00:27:36 +0200

    +Fragments backgrounds added

--- app/src/main/java/noname/astroweather/MoonFragment.java
+++ app/src/main/java/noname/astroweather/MoonFragment.java
@@ -1,18 +1,35 @@
 package noname.astroweather;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class MoonFragment extends Fragment {
+
+    TextView mView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(
                 R.layout.fragment_moon, container, false);
+
+        mView = (TextView) rootView.findViewById(R.id.ksiezyczek);
+
+        Configuration config = getResources().getConfiguration();
+        if (config.orientation == 2) {
+            mView.setBackgroundResource(R.drawable.moon_landscape);
+        } else if (config.orientation == 1) {
+            //pionowe
+            mView.setBackgroundResource(R.drawable.moon_portrait);
+        }
+
+
 
         return rootView;
     }
--- app/src/main/java/noname/astroweather/SunFragment.java
+++ app/src/main/java/noname/astroweather/SunFragment.java
@@ -1,21 +1,21 @@
 package noname.astroweather;
 
-import android.os.Build;
+
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import com.astrocalculator.AstroCalculator;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class SunFragment extends Fragment {
 
     TextView mView;
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -24,6 +24,16 @@ public class SunFragment extends Fragment {
                 R.layout.fragment_sun, container, false);
 
         mView = (TextView) rootView.findViewById(R.id.sloneczko);
+
+        Configuration config = getResources().getConfiguration();
+        if (config.orientation == 2) {
+            mView.setBackgroundResource(R.drawable.sun_landscape);
+        } else if (config.orientation == 1) {
+            //pionowe
+            mView.setBackgroundResource(R.drawable.sun_portrait);
+        }
+
+
 
         mView.setText("Nowa randomowa wartość");
 
