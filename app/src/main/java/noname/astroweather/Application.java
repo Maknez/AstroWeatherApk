275d3eb1b1c9270d23b3cd19899e26998edc56e6
Maknez <themaknez@gmail.com>
2018-04-20 23:56:39 +0200

    +Clock implemented +simple SunFragment access to object implemented

--- app/src/main/java/noname/astroweather/Application.java
+++ app/src/main/java/noname/astroweather/Application.java
@@ -1,45 +1,129 @@
 package noname.astroweather;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import java.text.DateFormat;
+import java.util.Calendar;
+
 
 
 public class Application extends AppCompatActivity {
 
+    TextView clockView;
     ViewPager mPager;
     PagerAdapter mPagerAdapter;
+    Fragment sunFragment, moonFragment;
+    private DateFormat simpleDateFormat;
+
+
+    public boolean checkSize(Configuration config) {
+        boolean xlarge = ((config.screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_XLARGE);
+        boolean large = ((config.screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_LARGE);
+        return (xlarge || large);
+    }
+
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putInt("currentFragment", mPager.getCurrentItem());
+        // outState.putInt("currentFragment", mPager.getCurrentItem());
     }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_application);
 
-        mPager = (ViewPager) findViewById(R.id.viewPager);
-
-        if(savedInstanceState != null) {
-            mPager.setCurrentItem(savedInstanceState.getInt("currentFragment"));
+        Configuration config = getResources().getConfiguration();
+        if (checkSize(config)) {
+            if (config.orientation == 2) {
+                setContentView(R.layout.activity_application_landscape_tablet);
+            } else if (config.orientation == 1) {
+                setContentView(R.layout.activity_application_portrait_tablet);
+            }
+        } else {
+            if (config.orientation == 2) {
+                setContentView(R.layout.activity_application_landscape_phone);
+            } else if (config.orientation == 1) {
+                setContentView(R.layout.activity_application_portrait_phone);
+                mPager = (ViewPager) findViewById(R.id.viewPager);
+                mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+                mPager.setAdapter(mPagerAdapter);
+            }
         }
+        clockView = (TextView) findViewById(R.id.clockOnScreen);
 
-        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
-        mPager.setAdapter(mPagerAdapter);
+        Thread myThread;
+        Runnable myRunnableThread = new Clock();
+        myThread = new Thread(myRunnableThread);
+        myThread.start();
+
+
+
 
     }
 
+    public void getClock() {
+
+        runOnUiThread(new Runnable() {
+            public void run() {
+                try {
+                    String curTime;
+                    long analogClockTime = Calendar.getInstance().getTimeInMillis();
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.setTimeInMillis(analogClockTime);
+                    int hour = calendar.get(Calendar.HOUR_OF_DAY);
+                    int minute = calendar.get(Calendar.MINUTE);
+                    int second = calendar.get(Calendar.SECOND);
+
+                    if (hour < 10) {
+                        curTime = ("0" + hour) + ":";
+                    } else {
+                        curTime = hour + ":";
+                    }
+                    if (minute < 10) {
+                        curTime = curTime + ("0" + minute) + ":";
+                    } else {
+                        curTime = curTime + minute + ":";
+                    }
+                    if (second < 10) {
+                        curTime = curTime + ("0" + second);
+                    } else {
+                        curTime = curTime + second;
+                    }
+
+                    clockView.setText(curTime);
+
+                } catch (Exception e) {
+                }
+            }
+        });
+    }
+
+    class Clock implements Runnable {
+        public void run() {
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    getClock();
+                    Thread.sleep(1000); // Pause of 1 Second
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                } catch (Exception e) {
+                }
+            }
+        }
+    }
+
+
     @Override
     public void onBackPressed() {
-
 
         if (mPager.getCurrentItem() == 0) {
             // If the user is currently looking at the first step, allow the system to handle the
@@ -54,7 +138,7 @@ public class Application extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_menu, menu);
-    return true;
+        return true;
     }
 
     @Override
@@ -79,10 +163,4 @@ public class Application extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-
-
-
-
-
-
 }
--- app/src/main/java/noname/astroweather/SunFragment.java
+++ app/src/main/java/noname/astroweather/SunFragment.java
@@ -1,22 +1,33 @@
 package noname.astroweather;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 import com.astrocalculator.AstroCalculator;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class SunFragment extends Fragment {
+
+    TextView mView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(
                 R.layout.fragment_sun, container, false);
 
+        mView = (TextView) rootView.findViewById(R.id.sloneczko);
+
+        mView.setText("Nowa randomowa wartość");
+
+
         return rootView;
     }
-
-
-
 }
