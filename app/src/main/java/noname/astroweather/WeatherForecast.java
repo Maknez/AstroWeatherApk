637b377d6fb787cf065584766ea09339727a95b4
Maknez <themaknez@gmail.com>
2018-05-28 14:38:10 +0200

    +BasicInfo fragment +WeatherForecast fragment +WindAndHumidity fragment

---
+++ app/src/main/java/noname/astroweather/WeatherForecast.java
@@ -0,0 +1,26 @@
+package noname.astroweather;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WeatherForecast extends Fragment {
+
+
+    public WeatherForecast() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.fragment_weather_forecast, container, false);
+        return rootView;
+    }
+
+}
---
+++ app/src/main/java/noname/astroweather/WindAndHumidity.java
@@ -0,0 +1,25 @@
+package noname.astroweather;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WindAndHumidity extends Fragment {
+
+
+    public WindAndHumidity() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.fragment_wind_and_humidity, container, false);
+        return rootView;
+    }
+
+}
