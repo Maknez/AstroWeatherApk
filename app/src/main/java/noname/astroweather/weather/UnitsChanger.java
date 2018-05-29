a174ae13899475db78c7454b9c5857d1dd2de139
Maknez <themaknez@gmail.com>
2018-05-30 00:43:42 +0200

    +mph -> kmph func +kmph -> mph func

--- app/src/main/java/noname/astroweather/weather/UnitsChanger.java
+++ app/src/main/java/noname/astroweather/weather/UnitsChanger.java
@@ -1,6 +1,6 @@
 package noname.astroweather.weather;
 
-public class TemperatureUnitsChanger {
+public class UnitsChanger {
 
     public int fahrenheitToCelsius(int fahrenheit) {
         return ((fahrenheit - 32) * 5 / 9);
@@ -8,5 +8,13 @@ public class TemperatureUnitsChanger {
 
     public int celsiusToFahrenheit(int celsius) {
         return ((celsius * 9 / 5) + 32);
+    }
+
+    public double milesPerHourTokilometersPerHour(double mph) {
+        return (mph * 1.609344);
+    }
+
+    public double kilometersPerHourToMilesPerHour(double kmph) {
+        return (kmph / 1.609344);
     }
 }
