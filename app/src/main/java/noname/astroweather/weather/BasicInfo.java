27ac148a33802a1a3f9ac68fd7ccb9c5b6e09bba
Maknez <themaknez@gmail.com>
2018-06-12 20:52:52 +0200

    +clean code

--- app/src/main/java/noname/astroweather/weather/BasicInfo.java
+++ app/src/main/java/noname/astroweather/weather/BasicInfo.java
@@ -7,15 +7,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import noname.astroweather.R;
-import noname.astroweather.weather.data.Channel;
-import noname.astroweather.weather.data.Item;
-import noname.astroweather.weather.data.interfaces.WeatherServiceCallback;
-import noname.astroweather.weather.data.YahooWeatherService;
 
 public class BasicInfo extends Fragment {
 
@@ -25,8 +19,7 @@ public class BasicInfo extends Fragment {
     private TextView airPressureTextView;
     private TextView descriptionTextView;
     private ImageView weatherImageView;
-
-    private SharedPreferences sharedPreferences;
+    private SharedPreferences sharedPreferencesWithCustomValues;
     private SharedPreferences offlineDataSharedPreferences;
 
     @Override
@@ -41,43 +34,43 @@ public class BasicInfo extends Fragment {
         ViewGroup rootView = (ViewGroup) inflater.inflate(
                 R.layout.fragment_basic_info, container, false);
 
-        initTextViews(rootView);
-        initImageView(rootView);
-        initSharedPreferences();
-
+        initializeTextViews(rootView);
+        initializeImageView(rootView);
+        initializeSharedPreferences();
         return rootView;
     }
 
-    private void initSharedPreferences() {
-        sharedPreferences = getActivity().getSharedPreferences("config.xml", 0);
+    private void initializeSharedPreferences() {
+        sharedPreferencesWithCustomValues = getActivity().getSharedPreferences("config.xml", 0);
         offlineDataSharedPreferences = getActivity().getSharedPreferences("offline_data.xml", 0);
     }
 
-    private void initImageView(ViewGroup rootView) {
-        weatherImageView = (ImageView) rootView.findViewById(R.id.weatherImage);
+    private void initializeImageView(ViewGroup rootView) {
+        weatherImageView = rootView.findViewById(R.id.weatherImage);
     }
 
-    private void initTextViews(ViewGroup rootView) {
-        cityTextView = (TextView) rootView.findViewById(R.id.cityText);
-        countryTextView = (TextView) rootView.findViewById(R.id.countryText);
-        temperatureTextView = (TextView) rootView.findViewById(R.id.temperatureText);
-        airPressureTextView = (TextView) rootView.findViewById(R.id.airPressureText);
-        descriptionTextView = (TextView) rootView.findViewById(R.id.descriptionText);
+    private void initializeTextViews(ViewGroup rootView) {
+        cityTextView = rootView.findViewById(R.id.cityText);
+        countryTextView = rootView.findViewById(R.id.countryText);
+        temperatureTextView = rootView.findViewById(R.id.temperatureText);
+        airPressureTextView = rootView.findViewById(R.id.airPressureText);
+        descriptionTextView = rootView.findViewById(R.id.descriptionText);
     }
 
     private void showDataFromSharedPreferences() {
-        weatherImageView.setImageResource(offlineDataSharedPreferences.getInt("resourceIDOffline", getResources().getIdentifier("weather_icon_" + 44, "drawable", getContext().getPackageName())));
-        cityTextView.setText(offlineDataSharedPreferences.getString("cityOffline", "cityName"));
-        countryTextView.setText(offlineDataSharedPreferences.getString("countryOffline", "countryName"));
-
         int temperatureInFarenheit = offlineDataSharedPreferences.getInt("temperatureInFarenheitOffline", 0);
         int temperatureInCelsius = offlineDataSharedPreferences.getInt("temperatureInCelsiusOffline", 0);
-        int temperatureUnit = sharedPreferences.getInt("Temperature_Unit", (getResources().getInteger(R.integer.Default_Temperature_Unit)));
+        int temperatureUnit = sharedPreferencesWithCustomValues.getInt("Temperature_Unit", (getResources().getInteger(R.integer.Default_Temperature_Unit)));
+
         if (temperatureUnit == 0) {
             temperatureTextView.setText(temperatureInCelsius + " " + getResources().getString(R.string.temperature_unit_celsius));
         } else if (temperatureUnit == 1) {
             temperatureTextView.setText(temperatureInFarenheit + " " + getResources().getString(R.string.temperature_unit_farenheit));
         }
+
+        weatherImageView.setImageResource(offlineDataSharedPreferences.getInt("resourceIDOffline", getResources().getIdentifier("weather_icon_" + 44, "drawable", getContext().getPackageName())));
+        cityTextView.setText(offlineDataSharedPreferences.getString("cityOffline", "cityName"));
+        countryTextView.setText(offlineDataSharedPreferences.getString("countryOffline", "countryName"));
         descriptionTextView.setText(offlineDataSharedPreferences.getString("descriptionOffline", "description"));
         airPressureTextView.setText(offlineDataSharedPreferences.getString("airPressureOffline", "airPressure") + " in");
     }
