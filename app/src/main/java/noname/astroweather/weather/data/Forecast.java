5cf0a2249cc598a9201b4fd9e678bb4cedf3e5b8
Maknez <themaknez@gmail.com>
2018-06-12 21:13:12 +0200

    +clean code

--- app/src/main/java/noname/astroweather/weather/data/Forecast.java
+++ app/src/main/java/noname/astroweather/weather/data/Forecast.java
@@ -4,7 +4,6 @@ import org.json.JSONObject;
 
 import noname.astroweather.weather.data.interfaces.JSONPopulator;
 
-//https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys
 public class Forecast implements JSONPopulator {
 
     private int code;
